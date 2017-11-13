070e9435caead358753668ee4e1cdc141a7f20e6
gaohuijue <gaohuijue@shinow.com>
2017-11-13 16:50:05 +0800

    增加index.

---
+++ src/EvtLine.js
@@ -0,0 +1,24 @@
+define(function (require) {
+
+    var zrUtil = require('zrender/lib/core/util');
+    var echarts = require('echarts/lib/echarts');
+    var PRIORITY = echarts.PRIORITY;
+
+    require('./LineSeries');
+    require('./LineView');
+
+    echarts.registerVisual(zrUtil.curry(
+        require('echarts/lib/visual/symbol'), 'evtline', 'circle', 'evtline'
+    ));
+    echarts.registerLayout(zrUtil.curry(
+        require('echarts/lib/layout/points'), 'evtline'
+    ));
+
+    // Down sample after filter
+    echarts.registerProcessor(PRIORITY.PROCESSOR.STATISTIC, zrUtil.curry(
+        require('echarts/lib/processor/dataSample'), 'evtline'
+    ));
+
+    // In case developer forget to include grid component
+    require('echarts/lib/component/gridSimple');
+});
